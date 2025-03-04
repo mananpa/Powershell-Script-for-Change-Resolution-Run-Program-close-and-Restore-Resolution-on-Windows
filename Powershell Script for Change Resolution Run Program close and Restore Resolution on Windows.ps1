ee62b5445c93932fae8cfc51d19b57adaeacac10
Manuel Pacheco Valdez <mananpa@hotmail.com>
2025-03-03 22:16:56 -0700

    Create Powershell Script for Change Resolution Run Program close and Restore Resolution on Windows.ps1

---
+++ Powershell Script for Change Resolution Run Program close and Restore Resolution on Windows.ps1
@@ -0,0 +1,23 @@
+# Application path
+$appPath = "C:\Program Files\TeamViewer\TeamViewer.exe"
+# New resolution
+$newWidth = 1400
+$newHeight = 1050
+# Save original resolution
+$originalResolution = Get-DisplayResolution
+
+# Launch the application
+Start-Process $appPath
+
+# Change the resolution
+Set-DisplayResolution -Width $newWidth -Height $newHeight
+
+# Wait for the application to close
+$process = Get-Process | Where-Object { $_.Path -eq $appPath }
+while ($process -ne $null) {
+    Start-Sleep -Seconds 5
+    $process = Get-Process | Where-Object { $_.Path -eq $appPath }
+}
+
+# Revert to the original resolution
+Set-DisplayResolution -Width 1920 -Height 1080
